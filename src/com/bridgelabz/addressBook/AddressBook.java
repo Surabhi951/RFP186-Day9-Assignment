09887266544bc7e80c07071ba54b6500370b73f8
Surabhi951 <surabhibhagat95@gmail.com>
2022-08-15 00:33:11 +0530

    Added: edited existing contact person

--- src/com/bridgelabz/addressBook/AddressBook.java
+++ src/com/bridgelabz/addressBook/AddressBook.java
@@ -35,4 +35,39 @@ public class AddressBook {
         System.out.println(contactPerson);
         System.out.println("Contact Added Successfully...");
     }
+
+    void editContact(){
+        boolean isContactFound=false;
+        System.out.println(AddressBook);
+        System.out.println("Enter Name to edit Contact");
+        String name=sc.next();
+        for (ContactPerson contactPerson : AddressBook) {
+            if (name.equalsIgnoreCase(contactPerson.getFirstName())) {
+                isContactFound=true;
+                System.out.print("Enter First Name :");
+                contactPerson.setFirstName(sc.next());
+                System.out.print("Enter Last Name :");
+                contactPerson.setLastName(sc.next());
+                System.out.print("Enter Address :");
+                contactPerson.setAddress(sc.next());
+                System.out.print("Enter City :");
+                contactPerson.setCity(sc.next());
+                System.out.print("Enter State :");
+                contactPerson.setState(sc.next());
+                System.out.print("Enter ZipCode :");
+                contactPerson.setZipCode(sc.nextInt());
+                System.out.print("Enter Phone Number :");
+                contactPerson.setPhoneNumber(sc.nextLong());
+                System.out.print("Enter email :");
+                contactPerson.setEmail(sc.next());
+                System.out.println(contactPerson);
+                break;
+            }
+        }
+        if (isContactFound){
+            System.out.println("Contact Updated Successfully..");
+        }else{
+            System.out.println("Oops...Contact not found");
+        }
+    }
 }
--- src/com/bridgelabz/addressBook/AddressBookMain.java
+++ src/com/bridgelabz/addressBook/AddressBookMain.java
@@ -11,13 +11,16 @@ public class AddressBookMain {
         boolean doExit=false;
         while (!doExit) {
             System.out.println("******************\nSelect Option :");
-            System.out.println("1.Add Contact\n2.Exit");
+            System.out.println("1.Add Contact\n2.Edit Contact\n3.Exit");
             int option = sc.nextInt();
             switch (option) {
                 case 1:
                     addressBook.addContact();
                     break;
                 case 2:
+                    addressBook.editContact();
+                    break;
+                case 3:
                     doExit = true;
                     break;
                 default:
